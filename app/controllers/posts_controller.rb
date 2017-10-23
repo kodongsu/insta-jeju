07bf3eb0b8d0c67fbb22f02a280f420b5212a824
Ko DongSu <ehdtn156@gmail.com>
2017-10-23 11:03:49 +0000

    171023

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,38 @@
+class PostsController < ApplicationController
+    
+    def index
+        @posts = Post.all
+    end
+    
+    def new
+        
+    end
+    
+    def create
+       post = Post.new
+       post.content = params[:content]
+       post.save
+       
+       redirect_to "/posts/#{post.id}"
+    end
+    
+    def show
+       @posts = Post.find(params[:id])
+    end
+    
+    def edit
+        @post = Post.find(params[:id])
+    end
+    
+    def update
+       post = Post.find(params[:id])
+       post.content = params[:content]
+       post.save
+       
+       redirect_to "/posts/#{post.id}"
+    end
+    
+    def destory
+        
+    end
+end
